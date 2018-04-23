be46e147811f08feb6f0662e100251a48c2d3410
Eric Stavarache <ericptst@gmail.com>
2018-04-23 14:09:16 +0300

    Add YCM setup

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ pushd build
 conan install ..
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
 cmake --build .
+
+popd
+ln -sf build/conan_ycm_extra_conf.py .ycm_extra_conf.py
+ln -sf build/conan_ycm_flags.json .
